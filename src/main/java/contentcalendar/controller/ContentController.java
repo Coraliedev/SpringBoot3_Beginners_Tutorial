168769287fed04d965c18a6357b50eb601e7cd39
CoralieDev <coralie.acquaert@gmail.com>
2023-05-23 16:36:42 +0200

    feat(controller): create get all contents method

--- src/main/java/contentcalendar/controller/ContentController.java
+++ src/main/java/contentcalendar/controller/ContentController.java
@@ -1,10 +1,25 @@
 package contentcalendar.controller;
 
+import contentcalendar.model.Content;
+import contentcalendar.repository.ContentCollectionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/content")
 public class ContentController {
+    private final ContentCollectionRepository repository;
 
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    public List<Content> findAll() {
+        return repository.findAll();
+    }
 }
