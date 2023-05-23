52389bae2744a71723d39a5a88e6d309117013d9
CoralieDev <coralie.acquaert@gmail.com>
2023-05-23 18:33:21 +0200

    build: add spring boot starter validation dependency

--- src/main/java/contentcalendar/model/Content.java
+++ src/main/java/contentcalendar/model/Content.java
@@ -1,9 +1,12 @@
 package contentcalendar.model;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.time.LocalDateTime;
 
 public record Content(
         Integer id,
+        @NotBlank
         String title,
         String description,
         Status status,
